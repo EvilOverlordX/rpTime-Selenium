fbe47a89084c966271cea31efc64cd21da3aee89
EvilOverlordX <johndemichele@gmail.com>
2013-11-05 13:46:48 -0800

    Added ClientMule

---
+++ src/main/java/com/roosterpark/rptime/selenium/mule/ClientMule.java
@@ -0,0 +1,76 @@
+package com.roosterpark.rptime.selenium.mule;
+
+import com.roosterpark.rptime.selenium.control.complex.form.CreateClientForm;
+import com.roosterpark.rptime.selenium.control.complex.navbar.AdminNavBar;
+import com.roosterpark.rptime.selenium.page.ClientPage;
+import com.roosterpark.rptime.selenium.page.HomePage;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * User: John
+ * Date: 11/5/13
+ * Time: 10:52 AM
+ */
+public class ClientMule {
+
+    private WebDriver driver;
+
+    private LoginMule loginMule;
+
+    public ClientMule(WebDriver driver) {
+        this.driver = driver;
+        loginMule = new LoginMule(driver);
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public ClientPage addLunchRequiredClient(String name, String dayOfWeek) {
+        return addClient(name, true, dayOfWeek);
+    }
+
+    public ClientPage addNonLunchRequiredClient(String name, String dayOfWeek) {
+        return addClient(name, false, dayOfWeek);
+    }
+
+    private ClientPage addClient(String name, boolean isLunchRequired, String dayOfWeek) {
+        HomePage homePage = loginMule.loginAsAdmin();
+        AdminNavBar navBar = homePage.getAdminNavBar();
+        ClientPage clientPage = navBar.clickClientsLink();
+        clientPage.clickNewButton();
+        CreateClientForm createClientForm = clientPage.getCreateClientForm();
+        createClientForm.enterName(name);
+        if(isLunchRequired) {
+            createClientForm.checkLunchRequired();
+        }
+        switch (dayOfWeek) {
+            case "Sunday":
+                createClientForm.selectSunday();
+                break;
+            case "Monday":
+                createClientForm.selectMonday();
+                break;
+            case "Tuesday":
+                createClientForm.selectTuesday();
+                break;
+            case "Wednesday":
+                createClientForm.selectWednesday();
+                break;
+            case "Thursday":
+                createClientForm.selectThursday();
+                break;
+            case "Friday":
+                createClientForm.selectFriday();
+                break;
+            case "Saturday":
+                createClientForm.selectSaturday();
+                break;
+            default:
+                createClientForm.selectMonday();
+        }
+        createClientForm.clickSave();
+        return clientPage;
+    }
+
+}
