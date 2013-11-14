919815da4856b8e35f06e40cdd0ff1737746bdcf
EvilOverlordX <johndemichele@gmail.com>
2013-11-14 13:30:54 -0800

    Updated ContractsPage to use new page controls

--- src/main/java/com/roosterpark/rptime/selenium/page/ContractsPage.java
+++ src/main/java/com/roosterpark/rptime/selenium/page/ContractsPage.java
@@ -1,7 +1,11 @@
 package com.roosterpark.rptime.selenium.page;
 
 import com.roosterpark.rptime.selenium.NavBarPageObject;
+import com.roosterpark.rptime.selenium.control.Button;
+import com.roosterpark.rptime.selenium.control.complex.form.CreateContractForm;
+import com.roosterpark.rptime.selenium.control.complex.list.contract.ContractEditList;
 import com.roosterpark.rptime.selenium.exception.NotDirectlyOpenableException;
+import com.roosterpark.rptime.selenium.timer.WaitForVisible;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -11,6 +15,8 @@ import org.openqa.selenium.WebDriver;
  */
 public class ContractsPage extends NavBarPageObject{
 
+    private ContractEditList contractEditList;
+
     public ContractsPage(WebDriver driver) {
         super(driver);
     }
@@ -19,4 +25,39 @@ public class ContractsPage extends NavBarPageObject{
     public void openPage() {
         throw new NotDirectlyOpenableException("Contracts page should not be opened directly.");
     }
+
+    public CreateContractForm getCreateContractForm() {
+        NewButton newButton = new NewButton(getWebDriver());
+        newButton.click();
+        return new CreateContractForm(getWebDriver());
+    }
+
+    public void initContractEditList() {
+        contractEditList = new ContractEditList(getWebDriver());
+        contractEditList.init();
+    }
+
+    public ContractEditList getContractEditList() {
+        return contractEditList;
+    }
+
+    private class NewButton extends Button<Void> {
+
+        private static final String NEW_BUTTON = "new";
+
+        private WaitForVisible waitForVisible;
+
+        public NewButton(WebDriver driver) {
+            super(driver, NEW_BUTTON);
+        }
+
+        @Override
+        public Void click() {
+            waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return null;
+        }
+    }
+
 }
