c1b0a51f0e105a645c6f0f6ce57d21c332ada21f
EvilOverlordX <johndemichele@gmail.com>
2013-11-18 11:32:22 -0800

    Added ContractsMule

---
+++ src/main/java/com/roosterpark/rptime/selenium/mule/ContractsMule.java
@@ -0,0 +1,91 @@
+package com.roosterpark.rptime.selenium.mule;
+
+import com.roosterpark.rptime.selenium.control.complex.form.CreateContractForm;
+import com.roosterpark.rptime.selenium.page.ClientPage;
+import com.roosterpark.rptime.selenium.page.ContractsPage;
+import com.roosterpark.rptime.selenium.page.HomePage;
+import com.roosterpark.rptime.selenium.page.WorkerPage;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * User: John
+ * Date: 11/15/13
+ * Time: 2:00 PM
+ */
+public class ContractsMule {
+
+    private WebDriver driver;
+    private ClientMule clientMule;
+    private WorkerMule workerMule;
+    private LoginMule loginMule;
+    private HomePage homePage;
+    ContractsPage contractsPage;
+
+    public ContractsMule(WebDriver driver) {
+        this.driver = driver;
+        clientMule = new ClientMule(driver);
+        workerMule = new WorkerMule(driver);
+        loginMule = new LoginMule(driver);
+    }
+
+    public void setHomePage(HomePage homePage) {
+        this.homePage = homePage;
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void login() {
+        homePage = loginMule.loginAsAdmin();
+    }
+
+    public void addWorker(String firstName, String lastName, String email, String startDate, boolean isSalaried) {
+        WorkerPage workerPage = null;
+        workerMule.setHomePage(homePage);
+        if (isSalaried) {
+            workerPage = workerMule.addSalariedWorker(firstName, lastName, email, startDate);
+        } else {
+            workerPage = workerMule.addHourlyWorker(firstName, lastName, email, startDate);
+        }
+        workerPage.setAdmin(true);
+        homePage = workerPage.getAdminNavBar().clickHomeLink();
+    }
+
+    public void addClient(String client, String dayOfWeek, boolean isLunchRequired) {
+        ClientPage clientPage = null;
+        clientMule.setHomePage(homePage);
+        if(isLunchRequired) {
+            clientPage = clientMule.addLunchRequiredClient(client, dayOfWeek);
+        } else {
+            clientPage = clientMule.addNonLunchRequiredClient(client, dayOfWeek);
+        }
+        clientPage.setAdmin(true);
+        homePage = clientPage.getAdminNavBar().clickHomeLink();
+    }
+
+    public ContractsPage createOnSiteContract(String client, String worker, String startDate, String endDate) {
+        return createContract(client, worker, true, startDate, endDate);
+    }
+
+    public ContractsPage createOffSiteContract(String client, String worker, String startDate, String endDate) {
+        return createContract(client, worker, false, startDate, endDate);
+    }
+
+    private ContractsPage createContract(String client, String worker, boolean isOnSite,
+                                         String startDate, String endDate) {
+        contractsPage = homePage.getAdminNavBar().clickContractsLink();
+        contractsPage.waitForContractsRedraw();
+        CreateContractForm createContractForm = contractsPage.getCreateContractForm();
+        if (isOnSite) {
+            createContractForm.checkOnSite();
+        }
+        createContractForm.selectClient(client);
+        createContractForm.selectWorker(worker);
+        createContractForm.enterStartDate(startDate);
+        createContractForm.enterEndDate(endDate);
+        createContractForm.clickSave();
+        return contractsPage;
+    }
+
+}
