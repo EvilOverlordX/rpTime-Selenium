5baaf1ce1445ebee7090fc4a672be1d7b6fa6b6a
EvilOverlordX <johndemichele@gmail.com>
2013-11-19 10:42:00 -0800

    Added page links to home page

--- src/main/java/com/roosterpark/rptime/selenium/page/HomePage.java
+++ src/main/java/com/roosterpark/rptime/selenium/page/HomePage.java
@@ -1,7 +1,10 @@
 package com.roosterpark.rptime.selenium.page;
 
 import com.roosterpark.rptime.selenium.NavBarPageObject;
+import com.roosterpark.rptime.selenium.control.Link;
+import com.roosterpark.rptime.selenium.control.finder.FindByHelper.ByXpath;
 import com.roosterpark.rptime.selenium.exception.NotDirectlyOpenableException;
+import com.roosterpark.rptime.selenium.timer.WaitForVisible;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -19,4 +22,74 @@ public class HomePage extends NavBarPageObject {
     public void openPage() {
         throw new NotDirectlyOpenableException("Home page should not be opened directly");
     }
+
+    public WorkerPage clickWorkersLink() {
+        WorkersLink workersLink = new WorkersLink(getWebDriver());
+        return workersLink.click();
+    }
+
+    public ClientPage clickClientsLink() {
+        ClientsLink clientsLink = new ClientsLink(getWebDriver());
+        return clientsLink.click();
+    }
+
+    public ContractsPage clickContractsLink() {
+        ContractsLink contractsLink = new ContractsLink(getWebDriver());
+        return contractsLink.click();
+    }
+
+    private class WorkersLink extends Link<WorkerPage> {
+
+        private static final String ID = "//div[@id='mainDiv']/div[@id='rpTimeWrapperDiv']/div/div/div/div/" +
+                                         "ul/li/a[@href='#/worker']";
+
+        public WorkersLink(WebDriver driver) {
+            super(driver, ID, new ByXpath());
+        }
+
+        @Override
+        public WorkerPage click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return new WorkerPage(getDriver());
+        }
+    }
+
+    private class ClientsLink extends Link<ClientPage> {
+
+        private static final String ID = "//div[@id='mainDiv']/div[@id='rpTimeWrapperDiv']/div/div/div/div/" +
+                "ul/li/a[@href='#/client']";
+
+        public ClientsLink(WebDriver driver) {
+            super(driver, ID, new ByXpath());
+        }
+
+        @Override
+        public ClientPage click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return new ClientPage(getDriver());
+        }
+    }
+
+    private class ContractsLink extends Link<ContractsPage> {
+
+        private static final String ID = "//div[@id='mainDiv']/div[@id='rpTimeWrapperDiv']/div/div/div/div/" +
+                "ul/li/a[@href='#/contract']";
+
+        public ContractsLink(WebDriver driver) {
+            super(driver, ID, new ByXpath());
+        }
+
+        @Override
+        public ContractsPage click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return new ContractsPage(getDriver());
+        }
+    }
+
 }
