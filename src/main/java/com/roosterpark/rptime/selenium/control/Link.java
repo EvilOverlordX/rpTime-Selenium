ea0fc4684ce7c2a0335c391372215247cd59d243
EvilOverlordX <johndemichele@gmail.com>
2013-11-20 11:32:54 -0800

    Updated to add second constructor

--- src/main/java/com/roosterpark/rptime/selenium/control/Link.java
+++ src/main/java/com/roosterpark/rptime/selenium/control/Link.java
@@ -1,5 +1,6 @@
 package com.roosterpark.rptime.selenium.control;
 
+import com.roosterpark.rptime.selenium.control.finder.FindByHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,6 +20,11 @@ public abstract class Link<T> implements Control {
     public Link(WebDriver driver, String elementId) {
         this.driver = driver;
         this.element = this.driver.findElement(By.linkText(elementId));
+    }
+
+    public Link(WebDriver driver, String elementId, FindByHelper helper) {
+        this.driver = driver;
+        this.element = this.driver.findElement(helper.find(elementId));
     }
 
     public abstract T click();
