7003f67f47dcebc4d8b482af62bccd8a00af8bb4
EvilOverlordX <johndemichele@gmail.com>
2013-11-21 15:40:39 -0800

    Added buttons to UserTimeSheetPage

--- src/main/java/com/roosterpark/rptime/selenium/page/UserTimeSheetPage.java
+++ src/main/java/com/roosterpark/rptime/selenium/page/UserTimeSheetPage.java
@@ -1,7 +1,11 @@
 package com.roosterpark.rptime.selenium.page;
 
 import com.roosterpark.rptime.selenium.BasicPageObject;
+import com.roosterpark.rptime.selenium.control.Button;
+import com.roosterpark.rptime.selenium.control.finder.FindByHelper.ByClass;
 import com.roosterpark.rptime.selenium.exception.NotDirectlyOpenableException;
+import com.roosterpark.rptime.selenium.page.popup.TimeSheetPopup;
+import com.roosterpark.rptime.selenium.timer.WaitForVisible;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -18,6 +22,50 @@ public class UserTimeSheetPage extends BasicPageObject {
     @Override
     public void openPage() {
         throw new NotDirectlyOpenableException("User Time Sheet page should not be opened directly.");
+    }
+
+    public void clickCreateButton() {
+        CreateButton createButton = new CreateButton(getWebDriver());
+        createButton.click();
+    }
+
+    public TimeSheetPopup clickDropDownButton() {
+        DropDownButton dropDownButton = new DropDownButton(getWebDriver());
+        return dropDownButton.click();
+    }
+
+    private class CreateButton extends Button<Void> {
+
+        private static final String ID = "new";
+
+        public CreateButton(WebDriver driver) {
+            super(driver, ID);
+        }
+
+        @Override
+        public Void click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return null;
+        }
+    }
+
+    private class DropDownButton extends Button<TimeSheetPopup> {
+
+        private static final String ID = "dropdown-toggle";
+
+        public DropDownButton(WebDriver driver) {
+            super(driver, ID, new ByClass()) ;
+        }
+
+        @Override
+        public TimeSheetPopup click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return null;
+        }
     }
 
 }
