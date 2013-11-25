c2010c6a61d21cc7788c6c94e31e109c39f0dfb3
EvilOverlordX <johndemichele@gmail.com>
2013-11-25 11:43:28 -0800

    Updated WorkerEditListRowGenerator

--- src/main/java/com/roosterpark/rptime/selenium/control/complex/list/generator/WorkerEditListRowGenerator.java
+++ src/main/java/com/roosterpark/rptime/selenium/control/complex/list/generator/WorkerEditListRowGenerator.java
@@ -2,6 +2,7 @@ package com.roosterpark.rptime.selenium.control.complex.list.generator;
 
 import com.roosterpark.rptime.selenium.control.complex.list.EditButton;
 import com.roosterpark.rptime.selenium.control.complex.list.worker.WorkerEditListRow;
+import com.roosterpark.rptime.selenium.control.complex.list.worker.WorkerLink;
 import com.roosterpark.rptime.selenium.timer.WaitForVisible;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -27,47 +28,28 @@ public class WorkerEditListRowGenerator {
     }
 
     public List<WorkerEditListRow> generate() {
-        boolean found = false;
         getElements();
         for (WebElement element : elements) {
-            if (element.findElements(By.xpath(".//span[@class='btn-group']/button")).size() != 0) {
-                if (!found) {
-                    WaitForVisible waitForVisible = new WaitForVisible(element);
-                    waitForVisible.waitForVisible(60L, 100L);
-                    EditButton editButton = new EditButton(driver, getEditButtonId(element));
-                    rows.add(new WorkerEditListRow(editButton, getName(element), getEmail(element)));
-                    found = true;
-                } else {
-                    EditButton editButton = new EditButton(driver, getEditButtonId(element));
-                    rows.add(new WorkerEditListRow(editButton, getName(element), getEmail(element)));
-                }
-            }
+            WorkerLink link = new WorkerLink(driver, getLinkId(element));
+            rows.add(new WorkerEditListRow(link, getEmail(element)));
         }
         return rows;
     }
 
     private void getElements() {
-        elements = driver.findElements(By.className("ng-binding"));
+        elements = driver.findElements(By.xpath("//div[@id='mainDiv']/div/div/div/div/div/div/div/div/ul/li"));
     }
 
-    private String getEditButtonId(WebElement element) {
-        List<WebElement> buttons = element.findElements(By.xpath(".//span[@class='btn-group']/button"));
-        for (WebElement button : buttons) {
-            String id = button.getAttribute("id");
-            if (id.startsWith("edit")) {
-                return id;
-            }
-        }
-        return null; // just in case...
-    }
-
-    private String getName(WebElement element) {
-        return element.getText();
+    private String getLinkId(WebElement element) {
+        String baseText = element.getText().trim();
+        String[] parts = baseText.split(" ");
+        return parts[0] + " " + parts[1];
     }
 
     private String getEmail(WebElement element) {
-        WebElement spanElement = element.findElement(By.xpath(".//span[@class='text-muted ng-binding']"));
-        return spanElement.getText().trim();
+        String baseText = element.getText().trim();
+        String[] parts = baseText.split(" ");
+        return parts[parts.length - 1];
     }
 
 }
