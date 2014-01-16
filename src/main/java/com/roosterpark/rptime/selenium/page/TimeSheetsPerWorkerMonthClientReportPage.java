4714d93dd02050eecdfc9e1ace605e966d0f0f38
EvilOverlordX <johndemichele@gmail.com>
2014-01-16 15:45:20 -0800

    Updated to add previous month and next month buttons

--- src/main/java/com/roosterpark/rptime/selenium/page/TimeSheetsPerWorkerMonthClientReportPage.java
+++ src/main/java/com/roosterpark/rptime/selenium/page/TimeSheetsPerWorkerMonthClientReportPage.java
@@ -1,7 +1,9 @@
 package com.roosterpark.rptime.selenium.page;
 
 import com.roosterpark.rptime.selenium.BasicPageObject;
+import com.roosterpark.rptime.selenium.control.Button;
 import com.roosterpark.rptime.selenium.exception.NotDirectlyOpenableException;
+import com.roosterpark.rptime.selenium.timer.WaitForVisible;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -11,6 +13,9 @@ import org.openqa.selenium.WebDriver;
  */
 public class TimeSheetsPerWorkerMonthClientReportPage extends BasicPageObject {
 
+    private static final String PREVIOUS_BUTTON_ID = "previousMonth";
+    private static final String NEXT_BUTTON_ID = "nextMonth";
+
     public TimeSheetsPerWorkerMonthClientReportPage(WebDriver driver) {
         super(driver);
     }
@@ -19,6 +24,48 @@ public class TimeSheetsPerWorkerMonthClientReportPage extends BasicPageObject {
     public void openPage() {
         throw new NotDirectlyOpenableException("Time Sheets per Worker/Month/Client Report page should not"
                                                + " be opened directly.");
+    }
+
+    public TimeSheetsPerWorkerMonthClientReportPage clickPreviousMonthButton() {
+        PreviousMonthButton previousMonthButton = new PreviousMonthButton();
+        return previousMonthButton.click();
+    }
+
+    public TimeSheetsPerWorkerMonthClientReportPage clickNextMonthButton() {
+        NextMonthButton nextMonthButton = new NextMonthButton();
+        return nextMonthButton.click();
+    }
+
+    private class PreviousMonthButton extends Button<TimeSheetsPerWorkerMonthClientReportPage> {
+
+        public PreviousMonthButton() {
+            super(getWebDriver(), PREVIOUS_BUTTON_ID);
+        }
+
+        @Override
+        public TimeSheetsPerWorkerMonthClientReportPage click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return new TimeSheetsPerWorkerMonthClientReportPage(getDriver());
+        }
+
+    }
+
+    private class NextMonthButton extends Button<TimeSheetsPerWorkerMonthClientReportPage> {
+
+        public NextMonthButton() {
+            super(getWebDriver(), NEXT_BUTTON_ID);
+        }
+
+        @Override
+        public TimeSheetsPerWorkerMonthClientReportPage click() {
+            WaitForVisible waitForVisible = new WaitForVisible(getElement());
+            waitForVisible.defaultWaitForVisible();
+            getElement().click();
+            return new TimeSheetsPerWorkerMonthClientReportPage(getDriver());
+        }
+
     }
 
 }
