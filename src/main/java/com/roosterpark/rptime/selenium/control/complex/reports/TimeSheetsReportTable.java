28669add8f3eb191b4abff821030f4234459be9a
EvilOverlordX <johndemichele@gmail.com>
2014-01-22 11:04:52 -0800

    Updated to add worker String, along with equals(), hashCode(), and toString() methods

--- src/main/java/com/roosterpark/rptime/selenium/control/complex/reports/TimeSheetsReportTable.java
+++ src/main/java/com/roosterpark/rptime/selenium/control/complex/reports/TimeSheetsReportTable.java
@@ -22,6 +22,7 @@ public class TimeSheetsReportTable {
     private Map<String, TimeSheetsReportRow> rowsByDate;
     private WebElement parentElement;
     private WebDriver driver;
+    private String worker;
 
     public TimeSheetsReportTable(WorkerLink workerLink, WebDriver driver, WebElement parentElement) {
         this.workerLink = workerLink;
@@ -36,6 +37,7 @@ public class TimeSheetsReportTable {
         for (TimeSheetsReportRow row : rows) {
             rowsByDate.put(row.getDate(), row);
         }
+        worker = workerLink.getElement().getText().trim();
     }
 
     public WorkerPage clickWorkerLink() {
@@ -48,6 +50,51 @@ public class TimeSheetsReportTable {
 
     public TimeSheetsReportRow getRowByDate(String date) {
         return rowsByDate.get(date);
+    }
+
+    public String getWorker() {
+        return worker;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TimeSheetsReportTable that = (TimeSheetsReportTable) o;
+
+        if (driver != null ? !driver.equals(that.driver) : that.driver != null) return false;
+        if (parentElement != null ? !parentElement.equals(that.parentElement) : that.parentElement != null)
+            return false;
+        if (rows != null ? !rows.equals(that.rows) : that.rows != null) return false;
+        if (rowsByDate != null ? !rowsByDate.equals(that.rowsByDate) : that.rowsByDate != null) return false;
+        if (worker != null ? !worker.equals(that.worker) : that.worker != null) return false;
+        if (workerLink != null ? !workerLink.equals(that.workerLink) : that.workerLink != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = workerLink != null ? workerLink.hashCode() : 0;
+        result = 31 * result + (rows != null ? rows.hashCode() : 0);
+        result = 31 * result + (rowsByDate != null ? rowsByDate.hashCode() : 0);
+        result = 31 * result + (parentElement != null ? parentElement.hashCode() : 0);
+        result = 31 * result + (driver != null ? driver.hashCode() : 0);
+        result = 31 * result + (worker != null ? worker.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeSheetsReportTable{" +
+                "workerLink=" + workerLink +
+                ", rows=" + rows +
+                ", rowsByDate=" + rowsByDate +
+                ", parentElement=" + parentElement +
+                ", driver=" + driver +
+                ", worker='" + worker + '\'' +
+                '}';
     }
 
 }
