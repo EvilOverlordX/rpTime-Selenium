17ceeca957b99bff18d7d6289759e1311a7691de
EvilOverlordX <johndemichele@gmail.com>
2014-01-28 14:40:08 -0800

    Refactored some support methods up to a new abstract base class BasicWorkerTest

--- src/test/java/com/roosterpark/rptime/selenium/admin/WorkerPageTest.java
+++ src/test/java/com/roosterpark/rptime/selenium/admin/WorkerPageTest.java
@@ -1,17 +1,9 @@
 package com.roosterpark.rptime.selenium.admin;
 
-import com.roosterpark.rptime.selenium.BasicSeleniumTest;
 import com.roosterpark.rptime.selenium.control.complex.form.CreateWorkerForm;
-import com.roosterpark.rptime.selenium.control.complex.list.worker.WorkerEditList;
-import com.roosterpark.rptime.selenium.control.complex.list.worker.WorkerEditListRow;
-import com.roosterpark.rptime.selenium.control.complex.navbar.AdminNavBar;
-import com.roosterpark.rptime.selenium.mule.LoginMule;
-import com.roosterpark.rptime.selenium.page.admin.HomePage;
 import com.roosterpark.rptime.selenium.page.admin.WorkerPage;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -19,7 +11,7 @@ import static org.junit.Assert.assertTrue;
  * Date: 10/28/13
  * Time: 1:57 PM
  */
-public class WorkerPageTest extends BasicSeleniumTest {
+public class WorkerPageTest extends BasicWorkerTest {
 
     private static final String FIRST_NAME = "foo-" + System.currentTimeMillis();
     private static final String SALARIED_FIRST_NAME = "s-" + FIRST_NAME;
@@ -34,19 +26,13 @@ public class WorkerPageTest extends BasicSeleniumTest {
     private static final String BAD_EMAIL = "bademail@roosterpark.com";
     private static final String BAD_START_DATE = "2013-01-01";
 
-    private LoginMule loginMule;
     private CreateWorkerForm workerForm;
-
-    @Before
-    public void setup() {
-        loginMule = new LoginMule(getDriver());
-    }
 
     @Test
     public void addHourlyWorkerTest() {
         WorkerPage workerPage = goToWorkerPage();
         workerPage.initWorkerForm();
-        workerForm = workerPage.getCreateWorkerForm();
+        setWorkerForm(workerPage.getCreateWorkerForm());
         workerFormHelper(HOURLY_FIRST_NAME, LAST_NAME, HOURLY_EMAIL, START_DATE, true);
         workerPage.pauseForRedraw();
         workerPage.initWorkerEditList();
@@ -58,7 +44,7 @@ public class WorkerPageTest extends BasicSeleniumTest {
     public void addSalariedWorkerTest() {
         WorkerPage workerPage = goToWorkerPage();
         workerPage.initWorkerForm();
-        workerForm = workerPage.getCreateWorkerForm();
+        setWorkerForm(workerPage.getCreateWorkerForm());
         workerFormHelper(SALARIED_FIRST_NAME, LAST_NAME, SALARIED_EMAIL, START_DATE, false);
         workerPage.pauseForRedraw();
         workerPage.initWorkerEditList();
@@ -106,31 +92,6 @@ public class WorkerPageTest extends BasicSeleniumTest {
         assertTrue("Worker form not here!", workerForm.doesFirstNameHaveText());
         assertTrue("Worker form not here!", workerForm.doesLastNameHaveText());
         workerPage.close();
-    }
-
-    private void workerFormHelper(String firstName, String lastName, String email, String startDate, boolean isHourly) {
-        workerForm.enterFirstName(firstName);
-        workerForm.enterLastName(lastName);
-        workerForm.enterEmail(email);
-        workerForm.enterStartDate(startDate);
-        if (isHourly) {
-            workerForm.checkHourly();
-        }
-        workerForm.clickSave();
-    }
-
-    private void verifyWorkerAdded(WorkerPage workerPage, String email) {
-        WorkerEditList workerEditList = workerPage.getWorkerEditList();
-        WorkerEditListRow row = workerEditList.getRowByEmail(email);
-        assertNotNull("Worker not added!", row);
-    }
-
-    private WorkerPage goToWorkerPage() {
-        HomePage homePage = loginMule.loginAsAdmin();
-        AdminNavBar navBar = homePage.getNavBar();
-        WorkerPage workerPage = navBar.clickWorkersLink();
-        workerPage.pauseForRedraw();
-        return workerPage;
     }
 
 }
